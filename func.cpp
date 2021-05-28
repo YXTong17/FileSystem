45bdc7b21c1fe721f99734ac21325012c4df34c1
计算机1808-佟懿轩-20184465 <48703739+EdmundTYX@users.noreply.github.com>
2021-05-28 15:42:23 +0800

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -1,35 +1,114 @@
-#include <iostream>
 #include "filesys.h"
+
+/*!
+ * 读磁盘i节点
+ * @param DINode 磁盘i节点项目
+ * @param di_number 磁盘i节点编号
+ */
+void dinode_read(struct DINode &DINode, unsigned int di_number) {
+    // 根据磁盘i节点编号，计算相应的磁盘块号与位置，因为每块存储16个磁盘i节点
+    unsigned int real_addr = 10 + di_number / 16;
+    unsigned int offset = di_number % 16;
+    char block[BLOCK_SIZE] = {0};
+    disk_read(block, (int) real_addr);
+    struct DINode buf[16];
+    memcpy(buf, block, BLOCK_SIZE);
+    memcpy(&DINode, &buf[offset], DINODE_SIZE);
+}
+
+/*!
+ * 写磁盘i节点
+ * @param DINode 磁盘i节点项目
+ * @param di_number 磁盘i节点编号
+ */
+void dinode_write(const struct DINode &DINode, unsigned int di_number) {
+    // 根据磁盘i节点编号，计算相应的磁盘块号与位置，因为每块存储16个磁盘i节点
+    unsigned int real_addr = 10 + di_number / 16;
+    unsigned int offset = di_number % 16;
+    char block[BLOCK_SIZE] = {0};
+    disk_read(block, (int) real_addr);
+    struct DINode buf[16];
+    memcpy(buf, block, BLOCK_SIZE);
+    // 修改并写回
+    memcpy(&buf[offset], &DINode, DINODE_SIZE); // 修改结构体
+    memcpy(block, buf, BLOCK_SIZE); // 包装成磁盘块
+    disk_write(block, (int) real_addr);
+}
 
 /*!
  * 在当前目录创建子目录
  * @param file_name 目录名
- * @return 磁盘i节点地址
+ * @return 磁盘i节点地址，失败会打印信息并返回0
  */
 unsigned int creat_directory(char file_name[F_N_SIZE]) {
-    // 找到当前目录
-    user_mem[cur_user].cur_dir;
-    DINode new_director{    // 创建子目录的磁盘i节点
+    if (bitmap.all()) {//所有i节点都被分配
+        cout << "失败：所有i节点都被分配" << endl;
+        return 0;
+    }
+    unsigned short umod_d = umod + 111;
+    // 创建子目录的磁盘i节点
+    struct DINode new_director = {
             .owner = cur_user,
             .group = 0,
             .file_type = 1,
-            .mode = umod,
-            .addr = disk.allocate_block(),
+            .mode = umod_d,
+            .addr = {disk.allocate_block()},
             .block_num = 1,
             .file_size = 0,
             .link_count = 0,
-            .last_time = time((time_t *) nullptr),
+            .last_time = time((time_t *) nullptr)
     };
-    // 把这个磁盘i节点写入
-    if (bitmap.all())   //所有i节点都被分配
-        return 0;
-    for (int i = 0; i < DINODE_COUNT; i++)
-        if (bitmap[i] == 0) {
-            bitmap[i] = 1;
+    // 找到当前目录的SFD
+    char block[BLOCK_SIZE] = {0};
+    disk_read(block, (int) user_mem[cur_user].cur_dir->addr[0]);
+    // 读成SFD结构体
+    struct SFD SFD[DIR_NUM];
+    memcpy(SFD, block, sizeof(SFD));
+    int index, index_i, flag = 1;
+    // 修改SFD，找到空闲磁盘i节点
+    for (index = 0; index < DIR_NUM; index++)
+        // 找到空闲SFD栏
+        if (SFD[index].di_number == 0) {
+            for (index_i = 0; index_i < DINODE_COUNT; index_i++)
+                // 找到空闲i节点
+                if (bitmap[index_i] == 0) {
+                    strcpy(SFD[index].file_name, file_name);
+                    SFD[index].di_number = index_i;
+                    bitmap[index_i] = true;
+                    flag = 0;
+                    break;
+                }
         }
-    disk_read();
+    if (flag) {
+        cout << "失败：目录已满" << endl;
+        return 0;
+    }
+    // 写磁盘i节点
+    dinode_write(new_director, index_i);
+    // SFD写回磁盘
+    memcpy(block, SFD, sizeof(SFD));
+    disk_write(block, (int) user_mem[cur_user].cur_dir->addr[0]);
+    return new_director.addr[0];
 }
 
-void mkdir() {
-
+/*!
+ * 由磁盘i节点生成对应的内存i节点
+ * @param INode 要修改的内存i节点
+ * @param DINode 磁盘i节点指针
+ * @param di_number 磁盘i节点编号
+ * @return 内存i节点指针
+ */
+void get_inode(struct INode &INode, const struct DINode &DINode, unsigned int di_number) {
+    INode.di_number = di_number;
+    INode.state = ' ';
+    INode.access_count = 0;
+    INode.owner = DINode.owner;
+    INode.group = DINode.group;
+    INode.file_type = DINode.file_type;
+    INode.mode = DINode.mode;
+    memcpy(INode.addr, DINode.addr, sizeof(INode.addr));
+    INode.block_num = DINode.block_num;
+    INode.file_size = DINode.file_size;
+    INode.link_count = DINode.link_count;
+    INode.last_time = DINode.last_time;
 }
