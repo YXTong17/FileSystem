449f9066d7ee636f8f2af291c73b53ed9664a76f
计算机1808-佟懿轩-20184465 <48703739+EdmundTYX@users.noreply.github.com>
2021-05-31 11:53:42 +0800

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -2,133 +2,143 @@
 #include <iostream>
 #include <cstring>
 
-// 创建磁盘并初始化基本目录
-void creat_disk() {
-    fp = fopen("disk", "wb");
+/*!
+ * 读磁盘i节点
+ * @param DINode 磁盘i节点项目
+ * @param di_number 磁盘i节点编号
+ */
+void dinode_read(struct DINode &DINode, unsigned int di_number) {
+    // 根据磁盘i节点编号，计算相应的磁盘块号与位置，因为每块存储16个磁盘i节点
+    unsigned int real_addr = 10 + di_number / 16;
+    unsigned int offset = di_number % 16;
     char block[BLOCK_SIZE] = {0};
-    // 创建一个128MB的空间作为磁盘
-    for (int i = 0; i < DISK_BLK; i++)
-        fwrite(block, BLOCK_SIZE, 1, fp);
-    fclose(fp);
-    fp = fopen("disk", "rb+");
-    // 初始化超级块
-    disk.free_all();
-    // 新建用户admin
-    strcpy(user[0].user_name, "admin");
-    strcpy(user[0].password, "admin");
-    user[0].user_id = 0;
-    user_count++;
-    // 用admin身份 创建一些必要的文件,这些文件有/ /bin /dev /etc /lib /tmp /user
-    // 创建根目录
-    struct DINode root = {  // 根目录的磁盘i节点
-            .owner = 0,     // admin
-            .group = 0,     // 无
-            .file_type = 1, // 目录文件
-            .mode = 777,    // 默认全部权限
-            .addr = {disk.allocate_block()},    // 数据区第一块
+    disk_read(block, (int) real_addr);
+    struct DINode buf[16];
+    memcpy(buf, block, BLOCK_SIZE);
+    memcpy(&DINode, &buf[offset], DINODE_SIZE);
+}
+
+/*!
+ * 写磁盘i节点
+ * @param DINode 磁盘i节点项目
+ * @param di_number 磁盘i节点编号
+ */
+void dinode_write(const struct DINode &DINode, unsigned int di_number) {
+    // 根据磁盘i节点编号，计算相应的磁盘块号与位置，因为每块存储16个磁盘i节点
+    unsigned int real_addr = 10 + di_number / 16;
+    unsigned int offset = di_number % 16;
+    char block[BLOCK_SIZE] = {0};
+    disk_read(block, (int) real_addr);
+    struct DINode buf[16];
+    memcpy(buf, block, BLOCK_SIZE);
+    // 修改并写回
+    memcpy(&buf[offset], &DINode, DINODE_SIZE); // 修改结构体
+    memcpy(block, buf, BLOCK_SIZE); // 包装成磁盘块
+    disk_write(block, (int) real_addr);
+}
+
+/*!
+ * 在当前目录创建子目录
+ * @param file_name 目录名
+ * @return 磁盘i节点地址，失败会打印信息并返回0
+ */
+unsigned int creat_directory(char *file_name) {
+    if (bitmap.all()) {//所有i节点都被分配
+        cout << "失败：所有i节点都被分配" << endl;
+        return 0;
+    }
+    unsigned short umod_d = umod + 111;
+    // 创建子目录的磁盘i节点
+    struct DINode new_directory = {
+            .owner = cur_user,
+            .group = 0,
+            .file_type = 1,
+            .mode = umod_d,
+            .addr = {disk.allocate_block()},
             .block_num = 1,
-            .file_size = 144,
+            .file_size = 0,
             .link_count = 0,
             .last_time = time((time_t *) nullptr)
     };
-    // 根目录放到系统打开文件表中
-    struct INode root_i{};
-    get_inode(root_i, root, 0);
-    memcpy(&sys_open_file[0], &root_i, sizeof(root_i));
-    sys_open_file_count++;
-    // i节点写入磁盘
-    bitmap[0] = true;
-    dinode_write(root, 0);
-    // 当前目录指向根目录
-    user_mem[cur_user].cur_dir = &sys_open_file[0];
-    strcpy(user_mem[cur_user].cwd, "/");
-    // 建立子目录
-    creat_directory((char *) "bin");
-    creat_directory((char *) "dev");
-    creat_directory((char *) "etc");
-    creat_directory((char *) "lib");
-    creat_directory((char *) "tmp");
-    creat_directory((char *) "user");
+    // 找到当前目录的SFD
+    char block[BLOCK_SIZE] = {0};
+    disk_read(block, (int) user_mem[cur_user].cur_dir->addr[0]);
+    // 读成SFD结构体
+    struct SFD SFD[DIR_NUM];
+    memcpy(SFD, block, sizeof(SFD));
+    int index, index_i, flag = 1, i;
+    // 修改SFD，找到空闲磁盘i节点
+    for (i = 0; i < DIR_NUM; i++) {
+        // 找到空闲SFD栏
+        if (SFD[i].di_number == 0) {
+            index = i;
+            flag = 0;
+            break;
+        }
+        if (strcmp(SFD[i].file_name, file_name) == 4) {
+            cout << "失败：命名重复" << endl;
+            return 0;
+        }
+    }
+    // 继续查重名
+    for (; i < DIR_NUM; i++) {
+        if (strcmp(SFD[i].file_name, file_name) == 4) {
+            cout << "失败：命名重复" << endl;
+            return 0;
+        }
+    }
+    if (flag) {
+        cout << "失败：目录已满" << endl;
+        return 0;
+    }
+    for (index_i = 0; index_i < DINODE_COUNT; index_i++)
+        // 找到空闲i节点
+        if (bitmap[index_i] == 0) {
+            strcpy(SFD[index].file_name, file_name);
+            SFD[index].di_number = index_i;
+            bitmap[index_i] = true;
+            break;
+        }
+    // 写磁盘i节点
+    dinode_write(new_directory, index_i);
+    // SFD写回磁盘
+    memcpy(block, SFD, sizeof(SFD));
+    disk_write(block, (int) user_mem[cur_user].cur_dir->addr[0]);
+    return new_directory.addr[0];
 }
 
-// 在引导块中保存数据
-void store() {
-    char block[BLOCK_SIZE] = {0}, *p = block;
-    // 用户表
-    memcpy(p, user, sizeof(user));
-    p += sizeof(user);
-    memcpy(p, &user_count, sizeof(user_count));
-    p += sizeof(user_count);
-    // 用户组
-    memcpy(p, group, sizeof(group));
-    p += sizeof(group);
-    memcpy(p, &group_count, sizeof(group_count));
-    p += sizeof(group_count);
-    // 默认权限码
-    memcpy(p, &umod, sizeof(umod));
-    // 超级块
-    disk.store_super_block();
-    disk_write(block, 0);
-    // bitmap
-    char bitmap_c[DINODE_COUNT] = {0};
-    p = bitmap_c;
-    memcpy(bitmap_c, &bitmap, sizeof(bitmap));
-    for (int i = 2; i < 10; i++) {
-        disk_write(p, i);
-        p += BLOCK_SIZE;
-    }
+/*!
+ * 由磁盘i节点生成对应的内存i节点
+ * @param INode 要修改的内存i节点
+ * @param DINode 磁盘i节点指针
+ * @param di_number 磁盘i节点编号
+ * @return 内存i节点指针
+ */
+void get_inode(struct INode &INode, const struct DINode &DINode, unsigned int di_number) {
+    INode.di_number = di_number;
+    INode.state = ' ';
+    INode.access_count = 0;
+    INode.owner = DINode.owner;
+    INode.group = DINode.group;
+    INode.file_type = DINode.file_type;
+    INode.mode = DINode.mode;
+    memcpy(INode.addr, DINode.addr, sizeof(INode.addr));
+    INode.block_num = DINode.block_num;
+    INode.file_size = DINode.file_size;
+    INode.link_count = DINode.link_count;
+    INode.last_time = DINode.last_time;
 }
 
-// 从引导块中恢复数据
-void restore() {
-    char block[BLOCK_SIZE] = {0}, *p = block;
-    disk_read(block, 0);
-    // 用户表
-    memcpy(user, p, sizeof(user));
-    p += sizeof(user);
-    memcpy(&user_count, p, sizeof(user_count));
-    p += sizeof(user_count);
-    // 用户组
-    memcpy(group, p, sizeof(group));
-    p += sizeof(group);
-    memcpy(&group_count, p, sizeof(group_count));
-    p += sizeof(group_count);
-    // 默认权限码
-    memcpy(&umod, p, sizeof(umod));
-    // 超级块
-    disk.restore_super_block();
-    // bitmap
-    char bitmap_c[DINODE_BLK] = {0};
-    p = bitmap_c;
-    for (int i = 2; i < 10; i++) {
-        disk_read(p, i);
-        p += sizeof(bitmap);
-    }
-    memcpy(&bitmap, bitmap_c, sizeof(bitmap));
-}
-
-// 系统初始化
-void init() {
-    bitmap.reset(); // 初始化bitmap
-    init_buf();
-    sys_open_file_count = 0;
-    user_count = 0;
-    group_count = 0;
-    cur_user = 0;   // admin
-    umod = 644;
-    fp = fopen("disk", "rb+");
-    if (!fp) {  // 如果没找到disk
-        cout << "检测不到磁盘，重建中...\n";
-        creat_disk();
-    } else {
-        /* 读取磁盘中的数据 */
-        restore();
-        // 读取根目录
-        struct DINode root{};
-        dinode_read(root, 0);
-        get_inode(sys_open_file[0], root, 0);
-        sys_open_file_count++;
-        user_mem[0].cur_dir = &sys_open_file[0];
-        strcpy(user_mem->cwd, "/");
-    }
+void ls() {
+    char block[BLOCK_SIZE] = {0};
+    disk_read(block, (int) user_mem[cur_user].cur_dir->addr[0]);
+    struct SFD SFD[DIR_NUM];
+    memcpy(SFD, block, sizeof(SFD));
+    int count = 0;
+    for (auto &index : SFD)
+        if (index.di_number != 0) {
+            cout << index.file_name << endl;
+            count++;
+        }
+    cout << "共" << count << "项" << endl;
 }
