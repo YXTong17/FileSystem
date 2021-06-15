b4d5b741579924f2d02784e6ff8226e02faf10c8
计算机1808-佟懿轩-20184465 <48703739+EdmundTYX@users.noreply.github.com>
2021-06-15 11:12:20 +0800

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -18,6 +18,10 @@ void creat_disk() {
     strcpy(user[0].password, "admin");
     user[0].user_id = 0;
     user_count++;
+    // 用户组0，包含前5个用户
+    group[0].group_id = 0;
+    for (int i = 0; i < 5; i++)
+        group[0].user_id[i] = i;
     // 用admin身份 创建一些必要的文件,这些文件有/ /bin /dev /etc /lib /tmp /user
     // 创建根目录
     struct DINode root = {  // 根目录的磁盘i节点
@@ -36,6 +40,14 @@ void creat_disk() {
     get_inode(root_i, root, 0);
     memcpy(&sys_open_file[0], &root_i, sizeof(root_i));
     sys_open_file_count++;
+    // 用户打开文件表第一项永远是根目录
+    strcpy(user_mem[0].OFD[0].file_dir, "/");
+    user_mem[0].OFD[0].flag = 1;
+    user_mem[0].OFD[0].rw_point = 0;
+    user_mem[0].OFD[0].inode_number = 0;
+    user_mem[0].file_count = 1;
+    user_mem[0].cur_dir = &sys_open_file[0];
+    strcpy(user_mem[0].cwd, "/");
     // i节点写入磁盘
     bitmap[0] = true;
     dinode_create(root, 0);
@@ -46,9 +58,26 @@ void creat_disk() {
     creat_directory((char *) "bin");
     creat_directory((char *) "dev");
     creat_directory((char *) "etc");
+    creat_directory((char *) "home");
     creat_directory((char *) "lib");
     creat_directory((char *) "tmp");
     creat_directory((char *) "user");
+    string s = "home";
+    instruct_cd(s);
+    creat_directory((char *) "admin");
+    s = "admin";
+    instruct_cd(s);
+    int a_di = touch("a.txt");
+    int a_i = (int) dinode_read(a_di);
+    string txt = "Hello World!\n";
+    sys_open_file[a_i].addr[0] = disk.allocate_block();
+    sys_open_file[a_i].block_num = 1;
+    sys_open_file[a_i].file_size = txt.length();
+    memcpy(block, txt.c_str(), txt.length());
+    disk_write(block, (int) sys_open_file[a_i].addr[0]);
+    dinode_write(a_di);
+    s = "/";
+    instruct_cd(s);
 }
 
 // 在引导块中保存数据
@@ -76,6 +105,19 @@ void store() {
     for (int i = 2; i < 10; i++) {
         disk_write(p, i);
         p += BLOCK_SIZE;
+    }
+    // 把系统文件打开表写回
+    for (auto &i:sys_open_file) {
+        if (i.di_number != 0) {
+            unsigned int real_addr = 10 + i.di_number / 16;
+            unsigned int offset = i.di_number % 16;
+            disk_read(block, (int) real_addr);
+            struct DINode buf[16];
+            memcpy(buf, block, BLOCK_SIZE);
+            memcpy(&buf[offset], &i.owner, DINODE_SIZE); // 修改
+            memcpy(block, buf, BLOCK_SIZE); // 包装成磁盘块
+            disk_write(block, (int) real_addr);
+        }
     }
 }
 
@@ -109,6 +151,7 @@ void restore() {
 
 // 系统初始化
 void init() {
+    init_display();
     bitmap.reset(); // 初始化bitmap
     init_buf();
     sys_open_file_count = 0;
@@ -119,13 +162,25 @@ void init() {
     fp = fopen("disk", "rb+");
     if (!fp) {  // 如果没找到disk
         cout << "检测不到磁盘，重建中...\n";
+        display("检测不到磁盘，重建中...");
         creat_disk();
     } else {
         /* 读取磁盘中的数据 */
         restore();
         // 读取根目录
-        dinode_read(0);
+        char block[BLOCK_SIZE] = {0};
+        disk_read(block, 10);
+        struct DINode buf{};
+        memcpy(&buf, block + 0, DINODE_SIZE);
+        get_inode(sys_open_file[0], buf, 0);
+        sys_open_file_count++;
+        // 用户打开文件表第一项永远是根目录
+        strcpy(user_mem[0].OFD[0].file_dir, "/");
+        user_mem[0].OFD[0].flag = 1;
+        user_mem[0].OFD[0].rw_point = 0;
+        user_mem[0].OFD[0].inode_number = 0;
+        user_mem[0].file_count = 1;
         user_mem[0].cur_dir = &sys_open_file[0];
-        strcpy(user_mem->cwd, "/");
+        strcpy(user_mem[0].cwd, "/");
     }
 }
